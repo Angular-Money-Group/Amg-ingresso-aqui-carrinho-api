127724e628fc8db20054b5947d7190db1f79e71f
roberthsx <roberthenrrique@gmail.com>
2024-03-18 15:37:38 -0300

    ajuste de horas

--- Amg-ingressos-aqui-carrinho-api/Dto/TransactionTicketDto.cs
+++ Amg-ingressos-aqui-carrinho-api/Dto/TransactionTicketDto.cs
@@ -37,7 +37,7 @@ namespace Amg_ingressos_aqui_carrinho_api.Dto
                 NameUser = transaction.IdPerson,
                 NameEvent = transaction?.Events?.Find(e => e.Id == transaction.IdEvent)?.Name ?? string.Empty,
                 PaymentMethod = transaction?.PaymentMethod?.TypePayment.ToString() ?? string.Empty,
-                PurchaseDate = transaction?.DateRegister.ToLocalTime().ToString("dd-MM-yyyy") ?? string.Empty,
+                PurchaseDate = transaction?.DateRegister.ToLocalTime().ToString("dd-MM-yyyy") ?? string.Empty, 
                 PurchaseTime = transaction?.DateRegister.ToLocalTime().ToString("hh:mm:ss")?? string.Empty,
                 SubTotal = transaction?.TotalValue ?? new decimal(0),
                 Tax = transaction?.Tax ?? new decimal(0),
